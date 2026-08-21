1c44f48dc5598cc0393068bf9978772e4b44a9c5
Karel Bilek <kb@karelbilek.com>
2026-08-21 13:56:48 +0200

    remove unused version

--- exporters/otlpmetrichttp/internal/version.go
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-package internal
-
-// Version is the current release version of the OpenTelemetry OTLP HTTP metric
-// exporter in use.
-const Version = "1.45.0"
